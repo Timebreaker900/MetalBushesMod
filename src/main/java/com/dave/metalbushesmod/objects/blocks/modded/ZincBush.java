206b6c34cd879c129af8cb53f82049f22b845030
Timebreaker900 <d.breuer2911@gmail.com>
2021-06-23 16:14:35 +0200

    Update ZincBush.java

--- src/main/java/com/dave/metalbushesmod/objects/blocks/modded/ZincBush.java
+++ src/main/java/com/dave/metalbushesmod/objects/blocks/modded/ZincBush.java
@@ -7,16 +7,18 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SweetBerryBushBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.TagCollectionManager;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ZincBush extends SweetBerryBushBlock {
 
@@ -43,8 +45,13 @@ public class ZincBush extends SweetBerryBushBlock {
 
             return ActionResultType.SUCCESS;
         } else if (i == 3) {
+            ITag<Item> tag = TagCollectionManager.getManager().getItemTags().get(new ResourceLocation("forge", "nuggets/zinc"));
+            //Ingredient item = Ingredient.fromTag(tag);
 
-            spawnAsEntity(worldIn, pos, new ItemStack(ItemInit.ZINC_NUGGET.get(), 2));
+            Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation("data/forge/tags/items", "nuggets/zinc"));
+
+            spawnAsEntity(worldIn, pos, new ItemStack(item, 2));
+            //spawnAsEntity(worldIn, pos, new ItemStack(ItemInit.ZINC_NUGGET.get(), 2));
             worldIn.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_SWEET_BERRIES_PICK_FROM_BUSH, SoundCategory.BLOCKS, 1.0f, 0.8F + worldIn.rand.nextFloat() * 0.4F);
             worldIn.setBlockState(pos, state.with(AGE, Integer.valueOf(1)), 2);
 
